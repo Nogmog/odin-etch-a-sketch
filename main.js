67abf2962f4419d7f82fa50f4e127e5051f21b83
Nogmog <iexplodeh@hotmail.com>
2020-08-03 03:13:00 +0100

    Adding mouseover logic to main.js

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
-const mainGrid = document.querySelector("#container")
+const mainGrid = document.querySelector("#container");
 for(i = 0; i < 256; i++){
     let newGrid = document.createElement("div");
-    newGrid.classList.add("grids")
-    mainGrid.appendChild(newGrid)
+
+    newGrid.classList.add("grids");
+    newGrid.addEventListener("mouseover", onHover);
+
+    mainGrid.appendChild(newGrid);
+}
+
+
+function onHover(e){
+    console.log(e)
+
 }
