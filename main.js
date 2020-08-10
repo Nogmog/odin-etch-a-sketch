39e094a01651e474a45256f1d762b288d40300f6
Nogmog <iexplodeh@hotmail.com>
2020-08-10 01:44:54 +0100

    New logic to hovering over item in main.js

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ for(i = 0; i < 256; i++){
     let newGrid = document.createElement("div");
 
     newGrid.classList.add("grids");
+    newGrid.setAttribute("id", i);
     newGrid.addEventListener("mouseover", onHover);
 
     mainGrid.appendChild(newGrid);
@@ -10,7 +11,17 @@ for(i = 0; i < 256; i++){
 
 
 function onHover(e){
-    console.log(e)
-    e.target.classList.add("grid-black")
+    let f = document.getElementById(e.target.id)
+    f.style.cssText = "background-color: black"
 
 }
+
+const info = document.querySelector("#info");
+const resetBtn = info.querySelector("#reset");
+
+resetBtn.addEventListener("mouseup", function(){
+    const grids = mainGrid.querySelectorAll("#grids");
+    for(i = 0; i < mainGrid.children.length; i++){
+    grids[i].classList.remove("grid-black");
+    }
+})
