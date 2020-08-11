975bdf20aefbe1949aca767436258c446469dc87
Nogmog <iexplodeh@hotmail.com>
2020-08-11 01:19:57 +0100

    Added basic logic to change grid size button, currently isn't fully operational

--- main.js
+++ main.js
@@ -1,23 +1,27 @@
 const mainGrid = document.querySelector("#container");
-for(i = 0; i < 256; i++){
-    let newGrid = document.createElement("div");
 
-    newGrid.classList.add("grids");
-    newGrid.setAttribute("id", i);
-    newGrid.addEventListener("mouseover", onHover);
+function makeGrid(num){
+    numGrids = num**2
+    for(i = 0; i < numGrids; i++){
+        let newGrid = document.createElement("div");
 
-    mainGrid.appendChild(newGrid);
+        newGrid.classList.add("grids");
+        newGrid.setAttribute("id", i);
+        newGrid.addEventListener("mouseover", onHover);
+
+        mainGrid.appendChild(newGrid);
+    }
 }
 
-
 function onHover(e){
-    let f = document.getElementById(e.target.id)
+    let f = document.getElementById(e.target.id);
     f.style.cssText = "background-color: black";
 
 }
 
 const info = document.querySelector("#info");
 const resetBtn = info.querySelector("#reset");
+const sizeBtn = info.querySelector("#size");
 
 resetBtn.addEventListener("mouseup", function(){
     let grids = mainGrid.querySelectorAll(".grids");
@@ -26,3 +30,16 @@ resetBtn.addEventListener("mouseup", function(){
         element.style.cssText = "";
     });
 })
+
+sizeBtn.addEventListener("mouseup", function(){
+    let gridSize = prompt("How big would you like your new grid?", 16)
+    console.log(gridSize)
+    if(typeof gridSize == "number" || gridSize < 100){
+        console.log("Passed")
+        makeGrid(gridSize)
+    }else{
+        alert("Put a number in less than 100")
+    }
+})
+
+makeGrid(16)
